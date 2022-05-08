ffc0018b8b319fb30f85f9d22863e30ad77709ed
KimHyoJin <hyojin03131995@gmail.com>
2022-05-08 13:04:18 +0900

    initialize project

---
+++ src/main/java/com/fast/campus/simplesns/SimpleSnsApplication.java
@@ -0,0 +1,13 @@
+package com.fast.campus.simplesns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fast/campus/simplesns/SimpleSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.campus.simplesns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
