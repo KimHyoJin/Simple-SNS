1f0bbd86d29de14b42dfbd181780d9155d53f7c5
KimHyoJin <hyojin03131995@gmail.com>
2022-06-05 00:29:45 +0900

    feat : ignore configuration which is not necessary now

--- src/main/java/com/fast/campus/simplesns/SimpleSnsApplication.java
+++ src/main/java/com/fast/campus/simplesns/SimpleSnsApplication.java
@@ -2,12 +2,13 @@ package com.fast.campus.simplesns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SimpleSnsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleSnsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSnsApplication.class, args);
+    }
 
 }
